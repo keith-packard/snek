e9c005fd1189e65b81c89c2bede44ed5698c4795
Keith Packard <keithp@keithp.com>
2019-01-17 02:33:37 -0800

    newt-duino: Discard all newt_error messages for now
    These land in the data segment and consume hundreds of bytes. Until we
    sort out how to get them into flash, we'll just remove them.
    
    At the same time, increase the heap to 640 bytes (wow!)
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newt-duino/newt-duino.h
+++ newt-duino/newt-duino.h
@@ -1,5 +1,6 @@
 #include <avr/pgmspace.h>
-#define NEWT_POOL	256
+#define NEWT_POOL	640
+#define NEWT_DEBUG	0
 #define strtof(a,b) strtod(a,b)
 #define VALUE_STACK_SIZE	8
 #define PARSE_STACK_SIZE	64
@@ -8,3 +9,7 @@
 #define PARSE_TABLE_FETCH_KEY(a) 	((parse_key_t) pgm_read_word(a))
 #define PARSE_TABLE_FETCH_TOKEN(a)	((token_t) pgm_read_byte(a))
 #define PARSE_TABLE_FETCH_PRODUCTION(a)	((uint8_t) pgm_read_byte(a))
+void
+newt_internal_error(char *format, ...);
+#define newt_error_name newt_internal_error
+#define newt_error(fmt, ...) newt_internal_error("error")
