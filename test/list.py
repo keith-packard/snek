ac5fff52f63a73dcc84ae5f99eb46a7fe1c1c72b
Keith Packard <keithp@keithp.com>
2019-01-17 23:01:07 -0800

    Make some reasonable list tests
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- test/list.py
+++ test/list.py
@@ -1,11 +1,32 @@
-()
-[]
-(1,)
+#
+# tuples don't have in-place operation,
+# so += makes a new tuple
+#
+
 a = ()
-b = []
-b += [1,2]
+b = a
+a += (1,2)
 
-if a != (): exit(1)
+if a != (1,2): exit(1)
+if b != (): exit(1)
+
+#
+# lists have in-place operation,
+# so += does not make a new list
+# while a + b does make a new list
+#
+
+a = []
+b = a
+c = a + [1,2]
+a += [1,2]
+
+if a != [1,2]: exit(1)
 if b != [1,2]: exit(1)
-    
+if c != [1,2]: exit(1)
 
+a[0] = 3
+
+if a != [3,2]: exit(1)
+if b != [3,2]: exit(1)
+if c != [1,2]: exit(1)
