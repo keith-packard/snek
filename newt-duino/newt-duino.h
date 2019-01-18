7e33e51e9d50af7d48b17362aab1e9a58c0f83ea
Keith Packard <keithp@keithp.com>
2019-01-17 21:28:40 -0800

    newt-duino: Use hooks to remove more const data from RAM
    This adds hacks for fprintf, sprintf and newt_error, along with the
    macros needed to remove builtin names from flashs.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newt-duino/newt-duino.h
+++ newt-duino/newt-duino.h
@@ -1,5 +1,5 @@
 #include <avr/pgmspace.h>
-#define NEWT_POOL	640
+#define NEWT_POOL	900
 #define NEWT_DEBUG	0
 #define strtof(a,b) strtod(a,b)
 #define VALUE_STACK_SIZE	8
@@ -9,7 +9,59 @@
 #define PARSE_TABLE_FETCH_KEY(a) 	((parse_key_t) pgm_read_word(a))
 #define PARSE_TABLE_FETCH_TOKEN(a)	((token_t) pgm_read_byte(a))
 #define PARSE_TABLE_FETCH_PRODUCTION(a)	((uint8_t) pgm_read_byte(a))
-void
-newt_internal_error(char *format, ...);
+#define ERROR_FETCH_FORMAT_CHAR(a)	((char) pgm_read_byte(a))
+
 #define newt_error_name newt_internal_error
-#define newt_error(fmt, ...) newt_internal_error("error")
+#define newt_error(fmt, args...) do {				\
+		static const char PROGMEM __fmt__[] = (fmt);	\
+		newt_internal_error(__fmt__, ## args);		\
+	} while (0)
+
+#define fprintf(file, fmt, args...) do {			\
+		static const char PROGMEM __fmt__[] = (fmt);	\
+		fprintf_P(file, __fmt__, ## args);		\
+	} while(0)
+
+#define sprintf_const(dst, fmt, args...) do {			\
+		static const char PROGMEM __fmt__[] = (fmt);	\
+		sprintf_P(dst, __fmt__, ##args);		\
+	} while(0)
+
+#define sprintf(dst, fmt, args...) do {			\
+	if (__builtin_const_p(fmt)) { \
+		static const char PROGMEM __fmt__[] = __builtin_const_p(fmt) ? (fmt) : ""; \
+		sprintf_P(dst, __fmt__, ##args);		\
+	} else { \
+		sprintf(dst, fmt, ##args);	\
+	} \
+	} while(0)
+
+#define NEWT_BUILTIN_NAMES_DECLARATION(n) PROGMEM n
+#define NEWT_BUILTIN_NAMES(a)	((uint8_t) pgm_read_byte(&newt_builtin_names[a]))
+#define NEWT_BUILTIN_NAMES_CMP(a,b)	strcmp_P(a,b)
+
+static inline const char *
+avr_newt_builtin_names_return(const uint8_t *bits)
+{
+	static char ret[32];
+	int i;
+	char c;
+	char *r = ret;
+
+	while ((*r++ = (char) pgm_read_byte(bits++)))
+		;
+	return ret;
+}
+
+static inline int
+avr_newt_builtin_names_len(const uint8_t *a)
+{
+	int len = 0;
+
+	while (pgm_read_byte(a++))
+		len++;
+	return len;
+}
+
+#define newt_builtin_names_return(a) avr_newt_builtin_names_return(a)
+#define newt_builtin_names_len(a) avr_newt_builtin_names_len(a)
