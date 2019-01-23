06f24709b63951222e41f9b72e2a59573257817a
Keith Packard <keithp@keithp.com>
2019-01-23 16:53:05 +1300

    newt-duino: Whitespace cleanups
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- newt-duino/newt-duino.h
+++ newt-duino/newt-duino.h
@@ -5,7 +5,7 @@
 #define VALUE_STACK_SIZE	8
 #define PARSE_STACK_SIZE	64
 #define NEWT_STACK		16
-#define PARSE_TABLE_DECLARATION(t) PROGMEM t
+#define PARSE_TABLE_DECLARATION(t) 	PROGMEM t
 #define PARSE_TABLE_FETCH_KEY(a) 	((parse_key_t) pgm_read_word(a))
 #define PARSE_TABLE_FETCH_TOKEN(a)	((token_t) pgm_read_byte(a))
 #define PARSE_TABLE_FETCH_PRODUCTION(a)	((uint8_t) pgm_read_byte(a))
@@ -27,8 +27,8 @@
 		sprintf_P(dst, __fmt__, ##args);		\
 	} while(0)
 
-#define NEWT_BUILTIN_NAMES_DECLARE(n) PROGMEM n
-#define NEWT_BUILTIN_NAMES(a)	((uint8_t) pgm_read_byte(&newt_builtin_names[a]))
+#define NEWT_BUILTIN_NAMES_DECLARE(n) 	PROGMEM n
+#define NEWT_BUILTIN_NAMES(a)		((uint8_t) pgm_read_byte(&newt_builtin_names[a]))
 #define NEWT_BUILTIN_NAMES_CMP(a,b)	strcmp_P(a,b)
 
 #define NEWT_BUILTIN_DECLARE(n)	PROGMEM n
@@ -40,10 +40,9 @@
 #define NEWT_BUILTIN_FUNC3(b) 	((newt_poly_t(*)(newt_poly_t, newt_poly_t, newt_poly_t)) pgm_read_word(&(b)->func3))
 #define NEWT_BUILTIN_FUNC4(b) 	((newt_poly_t(*)(newt_poly_t, newt_poly_t, newt_poly_t, newt_poly_t)) pgm_read_word(&(b)->func4))
 
-#define NEWT_ROOT_DECLARE(n) PROGMEM n
-#define NEWT_ROOT_TYPE(n) ((const newt_mem_t *) pgm_read_word(&(n)->type))
-#define NEWT_ROOT_ADDR(n) ((void **) pgm_read_word(&(n)->addr))
-
+#define NEWT_ROOT_DECLARE(n)	PROGMEM n
+#define NEWT_ROOT_TYPE(n) 	((const newt_mem_t *) pgm_read_word(&(n)->type))
+#define NEWT_ROOT_ADDR(n) 	((void **) pgm_read_word(&(n)->addr))
 
 static inline const char *
 avr_newt_builtin_names_return(const uint8_t *bits)
@@ -71,7 +70,7 @@ avr_newt_builtin_names_len(const uint8_t *a)
 #define newt_builtin_names_return(a) avr_newt_builtin_names_return(a)
 #define newt_builtin_names_len(a) avr_newt_builtin_names_len(a)
 
-#define NEWT_MEM_DECLARE(n) PROGMEM n
+#define NEWT_MEM_DECLARE(n) 	PROGMEM n
 #define NEWT_MEM_SIZE(m)	((newt_offset_t (*)(void *addr)) pgm_read_word(&(m)->size))
 #define NEWT_MEM_MARK(m)	((void (*)(void *addr)) pgm_read_word(&(m)->mark))
 #define NEWT_MEM_MOVE(m)	((void (*)(void *addr)) pgm_read_word(&(m)->move))
