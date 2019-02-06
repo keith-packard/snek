2d108e5f615ea85edfe6d356a3190b1225cfac58
Keith Packard <keithp@keithp.com>
2019-02-06 00:07:03 -0800

    Add some snek-duino examples
    blink.py uses PWM output to gently modulate an LED.
    track-light.py shows how to hook analog input to a PWM output.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ examples/blink.py
@@ -0,0 +1,33 @@
+#
+# Copyright © 2019 Keith Packard <keithp@keithp.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+
+led = 3
+
+def up():
+    for i in range(0,1,0.001):
+        setpower(i)
+
+def down():
+    for i in range(1,0,-0.001):
+        setpower(i)
+
+def blink(n):
+    talkto(led)
+    setpower(0)
+    on()
+    for i in range(n):
+        up()
+        down()
+    off()
+
