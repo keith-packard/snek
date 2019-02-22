256565b04102c4406d4d20e2f21728115c631211
Keith Packard <keithp@keithp.com>
2019-02-21 23:35:32 -0800

    Add morse code example for Arduino
    Blinks the standard LED.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ examples/morse.py
@@ -0,0 +1,45 @@
+#
+# Morse code example
+#
+
+T = 1.2 / 12
+
+a=('.-', '-...', '-.-.', '-..', '.', '..-.', '--.',
+   '....', '..', '.---', '-.-', '.-..', '--', '-.',
+   '---', '.--.', '--.-', '.-.', '...', '-', '..-',
+   '...-', '.--', '-..-', '-.--', '--..')
+n=('-----', '.----', '..---', '...--', '....-',
+   '.....', '-....', '--...', '---..', '----.')
+
+def d(l):
+    l = ord(l)
+    if 97 <= l and l <= 122:
+        return a[l-97]
+    return n[l-48]
+
+def l(c):
+    es = d(c)
+    for e in es:
+        if e == '.':
+            onfor(T)
+            time.sleep(T)
+        else:
+            onfor(3*T)
+            time.sleep(T)
+    time.sleep(3*T)
+
+def m(t):
+    for c in t:
+        if c == ' ':
+            time.sleep(6*T)
+        elif c == '.':
+            time.sleep(12*T)
+        else:
+            l(c)
+
+def cq():
+    talkto(13)
+    while True:
+        m("cq cq de kd7sqg.")
+
+cq()
