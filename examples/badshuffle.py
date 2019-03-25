c8db3173ff2c8e49bebf3edfcdc3703ae767b000
Keith Packard <keithp@keithp.com>
2019-03-24 19:20:18 -0700

    Add badshuffle example
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ examples/badshuffle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import random
+
+# "Shuffle" a list, but *wrong*.
+
+narray = 16
+a = [0] * narray
+h = [0] * narray
+
+for _ in range(10000):
+    for i in range(narray):
+        a[i] = i
+    for i in range(narray):
+        j = random.randrange(narray)
+        tmp = a[j]
+        a[j] = a[i]
+        a[i] = tmp
+    for i in range(narray):
+        if a[i] == narray - 1:
+            h[i] += 1
+            break
+
+print(h)
