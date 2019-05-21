e7b5c9dc684b4db1e3bb9f24e0b39f76cecbef80
Keith Packard <keithp@keithp.com>
2019-05-21 12:57:03 -0700

    examples: Create one-neopixel example, save old one as neopixels
    The existing neopixel.py example uses 10 devices, which only the
    Circuit Playground Express board has. Provide a simpler one-neopixel
    version and call that neopixel.py leaving the old 10-neopixel example
    as neopixels.py
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- examples/neopixel.py
+++ examples/neopixel.py
@@ -1,9 +1,9 @@
 #
-# Copyright © 2019 Keith Packard <keithp@keithp.com>
+# Copyright  2019 Keith Packard <keithp@keithp.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful, but
@@ -11,52 +11,30 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-#
-# Rotate a rainbow through a set of pixels
-#
 
-num_pixels = 10
-bright = 0.05
-pixels = [0] * num_pixels
-for i in range(num_pixels):
-    pixels[i] = [0,0,0]
+bright = 0.1
 
-def wheel(i, pos):
-    if pos < 0 or pos > 255:
-        r = 0
-        g = 0
-        b = 0
-    elif pos < 85:
-        r = pos/85
+def rainbow(pos):
+    if pos < 1:
+        r = pos
         g = 1 - r
         b = 0
-    elif pos < 170:
-        b = (pos - 85) / 85
+    elif pos < 2:
+        b = pos - 1
         r = 1 - b
         g = 0
     else:
-        g = (pos - 170) / 85
+        g = pos - 2
         r = 0
         b = 1 - g
-    pixels[i][0] = r * bright
-    pixels[i][1] = g * bright
-    pixels[i][2] = b * bright
+    neopixel(((bright*r,bright*g,bright*b),))
 
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            pixel_index = (i * 256 // num_pixels) + j
-            wheel(i, pixel_index & 255)
-        neopixel(pixels)
-        time.sleep(wait)
-
-def cycles():
+def blink():
     talkto(NEOPIXEL)
     while True:
-        rainbow_cycle(0.001)
+        for pos in range(0,3,0.01):
+            rainbow(pos)
+            time.sleep(0.003)
 
-cycles()
+blink()
+
---
+++ examples/neopixels.py
@@ -0,0 +1,62 @@
+#
+# Copyright © 2019 Keith Packard <keithp@keithp.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+#
+# Rotate a rainbow through a set of pixels
+#
+
+num_pixels = 10
+bright = 0.05
+pixels = [0] * num_pixels
+for i in range(num_pixels):
+    pixels[i] = [0,0,0]
+
+def wheel(i, pos):
+    if pos < 0 or pos > 255:
+        r = 0
+        g = 0
+        b = 0
+    elif pos < 85:
+        r = pos/85
+        g = 1 - r
+        b = 0
+    elif pos < 170:
+        b = (pos - 85) / 85
+        r = 1 - b
+        g = 0
+    else:
+        g = (pos - 170) / 85
+        r = 0
+        b = 1 - g
+    pixels[i][0] = r * bright
+    pixels[i][1] = g * bright
+    pixels[i][2] = b * bright
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            wheel(i, pixel_index & 255)
+        neopixel(pixels)
+        time.sleep(wait)
+
+def cycles():
+    talkto(NEOPIXEL)
+    while True:
+        rainbow_cycle(0.001)
+
+cycles()
