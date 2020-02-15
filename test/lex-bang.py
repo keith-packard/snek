2f00cdcd5679cf0d6acf980ff62319bc6ac01fc9
Keith Packard <keithp@keithp.com>
2020-02-15 13:58:41 -0800

    Add test for lexer returning error on invalid token
    '!' is not a valid token, so the lexer should return an error when it
    occurs in the input.
    
    Reported-by: Helmut Grohne <helmut@subdivi.de>
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ test/lex-bang.py
@@ -0,0 +1,21 @@
+#
+# Copyright © 2020 Keith Packard <keithp@keithp.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# Check for lex failure for token '!'
+
+!1
+exit(0)
