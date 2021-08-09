36f5e5aa79a188d8b77d3ef8fdd29fbb0f195d12
Keith Packard <keithp@keithp.com>
2021-08-09 09:47:52 -0700

    atmel: Simplify memory configuration
    Make all 328p devices use a common memory setup, as well as 32u4.
    Make mega use the standard memory setup.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- chips/avr/snek-avr.h
+++ chips/avr/snek-avr.h
@@ -30,8 +30,10 @@ sqrtf(float x) {
 
 #define SNEK_DEBUG	0
 #define strtof(a,b) strtod(a,b)
+#define SNEK_POOL		1024
+#define SNEK_MAX_TOKEN		63
 #define VALUE_STACK_SIZE	16
-#define PARSE_STACK_SIZE	56
+#define PARSE_STACK_SIZE	128
 #define SNEK_STACK		32
 #define PARSE_TABLE_DECLARATION(t) 	PROGMEM t
 #define PARSE_TABLE_FETCH_TOKEN(a)	((token_key_t) pgm_read_byte(a))
