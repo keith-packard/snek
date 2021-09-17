b425a5710bdde44977adeab1b96616e651d2bac7
Keith Packard <keithp@keithp.com>
2021-09-16 17:25:07 -0700

    Use new lola PARSE_TOP feature
    Replace some static variables that aren't used outside of the parse
    function with locals declared in the PARSE_TOP fragment.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- snek-parse.c
+++ snek-parse.c
@@ -17,9 +17,6 @@
 bool snek_interactive;
 
 bool snek_parse_middle;
-static uint8_t for_depth;
-
-static bool snek_print_val;
 
 uint8_t snek_parse_nformal;
 uint8_t snek_parse_nnamed;
@@ -38,6 +35,10 @@ snek_token_val_t snek_token_val;
 #else
 #define snek_no_dict 0
 #endif
+
+#define PARSE_TOP \
+	uint8_t for_depth = 0; \
+	bool snek_print_val = 0;
 
 #define GRAMMAR_TABLE
 #ifdef PARSE_DEBUG
@@ -225,7 +226,6 @@ snek_parse(void)
 		/* Reset parse state */
 		snek_parse_middle = false;
 		value_stack_p = 0;
-		for_depth = 0;
 
 		/* Reset codegen state */
 		snek_code_reset();
